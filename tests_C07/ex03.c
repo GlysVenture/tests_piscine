1e73967d0a6ae8ca531b3c0081de27ded5ec57a7
Tadeusz Kondracki <tkondrac@student.42lausanne.ch>
2021-08-13 14:42:34 +0200

    C07: better test for ex03

--- tests_C07/ex03.c
+++ tests_C07/ex03.c
@@ -11,8 +11,14 @@ int	main(void)
 	strs[0] = "premier";
 	strs[1] = "";
 	strs[2] = "3eme";
+	dest = ft_strjoin(0, strs, ":un separateur:");
+	printf("premier:un separateur::un separateur:3eme\n");
+	printf("%s\n", dest);
+	free(dest);
+	strs = NULL;
 	dest = ft_strjoin(3, strs, ":un separateur:");
 	printf("premier:un separateur::un separateur:3eme\n");
 	printf("%s\n", dest);
+	free(dest);
 	return (0);
 }
